d32dc894db1ea0dec19f7b231ee8f73e7696d2ce
Mael Vernoux <mael.vernoux@gmail.com>
2026-02-21 18:03:41 +0100

    add constant for WIDTH and HEIHT of the display window

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,18 @@ use std::i32;
 
 use bracket_lib::{prelude::*, random};
 
+// Window Size 
+const WIDTH: usize = 100; 
+const HEIGHT: usize = 60;
+
 struct State {
-    map : [[bool; 50] ; 80],
+    map : [[bool; HEIGHT] ; WIDTH],
 }
 
 /// print the map in the terminal
-fn print_map(map : [[bool; 50] ; 80], ctx : &mut BTerm){
-    for x in 0..80{
-        for y in 0..50{
+fn print_map(map : [[bool; HEIGHT] ; WIDTH], ctx : &mut BTerm){
+    for x in 0..WIDTH{
+        for y in 0..HEIGHT{
             if map[x][y]{
                 ctx.set(x, y, (0, 0, 0), (0, 0, 255), 0);
             }
@@ -37,7 +41,7 @@ fn bool_to_int(b: bool)-> i32{
 
 /// Count the number of neighbours of the point (x, y) and return true if 
 /// 3*count_same_neighbours >= count_possible_neighbours and map[x][y] equal true
-fn enough_neighbours(x: usize, y: usize, map : [[bool; 50] ; 80]) -> bool{
+fn enough_neighbours(x: usize, y: usize, map : [[bool; HEIGHT] ; WIDTH]) -> bool{
     let mut count_same_neighbours = 0;
     let mut count_possible_neighbours = 0;
     
@@ -50,7 +54,7 @@ fn enough_neighbours(x: usize, y: usize, map : [[bool; 50] ; 80]) -> bool{
         count_same_neighbours += bool_to_int(map[x][y] && map[x-1][y]);
         count_possible_neighbours += 1;
 
-        if y != 49{
+        if y != HEIGHT-1{
             count_same_neighbours += bool_to_int(map[x][y] && map[x-1][y+1]);
             count_possible_neighbours += 1;
         }
@@ -61,12 +65,12 @@ fn enough_neighbours(x: usize, y: usize, map : [[bool; 50] ; 80]) -> bool{
         count_possible_neighbours += 1;
     }
 
-    if y != 49{
+    if y != HEIGHT-1{
         count_same_neighbours += bool_to_int(map[x][y] && map[x][y+1]);
         count_possible_neighbours += 1;
     }
 
-    if x != 79{
+    if x != WIDTH-1{
         if y != 10 {
             count_same_neighbours += bool_to_int(map[x][y] && map[x+1][y-1]);
             count_possible_neighbours += 1;
@@ -75,7 +79,7 @@ fn enough_neighbours(x: usize, y: usize, map : [[bool; 50] ; 80]) -> bool{
         count_same_neighbours += bool_to_int(map[x][y] && map[x+1][y]);
         count_possible_neighbours += 1;
 
-        if y != 49{
+        if y != HEIGHT-1{
             count_same_neighbours += bool_to_int(map[x][y] && map[x+1][y+1]);
             count_possible_neighbours += 1;
         }
@@ -85,14 +89,14 @@ fn enough_neighbours(x: usize, y: usize, map : [[bool; 50] ; 80]) -> bool{
 }
 
 /// Generate a tab that represent the map of our game with randomize generation 
-/// and game of life algorithme to make a map more realistic
-fn generate_map()-> [[bool; 50] ; 80] {
+/// and game of life algorithm to make a map more realistic
+fn generate_map()-> [[bool; HEIGHT] ; WIDTH] {
     let mut seed = random::RandomNumberGenerator::new();
-    let mut map_rand = [[false; 50]; 80];
+    let mut map_rand = [[false; HEIGHT]; WIDTH];
 
     // generate our first random map 
-    for x in 0..80{
-        for y in 10..50{
+    for x in 0..WIDTH{
+        for y in 10..HEIGHT{
             map_rand[x][y] = seed.rand::<i32>() % 2 == 0; 
         }
     }
@@ -100,17 +104,17 @@ fn generate_map()-> [[bool; 50] ; 80] {
     // We increase the realism of our generation with a check 
     // of the neighbours of a pixel to determine if their is enough neighbours 
     // of the same state around to stay in this state
-    let mut map = [[false; 50]; 80];
-    for x in 0..80{
-        for y in 10..50{
+    let mut map = [[false; HEIGHT]; WIDTH];
+    for x in 0..WIDTH{
+        for y in 10..HEIGHT{
             map[x][y] = enough_neighbours(x, y, map_rand); //true if their is enough neighbours of the same color
         }
     }
 
     // And to increase the effect of our generation, 
     // we repeat the last operation 
-    for x in 0..80{
-        for y in 10..50{
+    for x in 0..WIDTH{
+        for y in 10..HEIGHT{
             map[x][y] = enough_neighbours(x, y, map);
         }
     }
@@ -119,7 +123,7 @@ fn generate_map()-> [[bool; 50] ; 80] {
 }
 
 fn main() -> BError {
-    let context = BTermBuilder::simple80x50()
+    let context = BTermBuilder::simple(WIDTH, HEIGHT).unwrap()
         .with_title("First Map")
         .build()?;
 
