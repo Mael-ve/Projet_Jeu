441d6f943670abb1dc4f4141df41805cbccfc791
Mael Vernoux <mael.vernoux@gmail.com>
2026-02-22 14:41:48 +0100

    good generation map

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use std::i32;
 use bracket_lib::{prelude::*, random};
 
 // Window Size 
-const WIDTH: usize = 100; 
-const HEIGHT: usize = 60;
+const WIDTH: usize = 150; 
+const HEIGHT: usize = 100;
 
 struct State {
     map : [[bool; HEIGHT] ; WIDTH],
@@ -39,83 +39,74 @@ fn bool_to_int(b: bool)-> i32{
     return 0;
 }
 
-/// Count the number of neighbours of the point (x, y) and return true if 
-/// 3*count_same_neighbours >= count_possible_neighbours and map[x][y] equal true
-fn enough_neighbours(x: usize, y: usize, map : [[bool; HEIGHT] ; WIDTH]) -> bool{
-    let mut count_same_neighbours = 0;
-    let mut count_possible_neighbours = 0;
+/// Count the number of neighbours with the value true of the point (x, y) and return true if 
+/// there is more than comparaison_level neighbours with this value
+fn enough_neighbours(x: usize, y: usize, map : [[bool; HEIGHT] ; WIDTH], comparaison_level : i32) -> bool{
+    let mut count_true_neighbours = 0;
     
     if x != 0 {
-        if y != 10 {
-            count_same_neighbours += bool_to_int(map[x][y] && map[x-1][y-1]);
-            count_possible_neighbours += 1;
+        if y != 0 {
+            count_true_neighbours += bool_to_int(map[x-1][y-1]);
         }
 
-        count_same_neighbours += bool_to_int(map[x][y] && map[x-1][y]);
-        count_possible_neighbours += 1;
+        count_true_neighbours += bool_to_int(map[x-1][y]);
 
         if y != HEIGHT-1{
-            count_same_neighbours += bool_to_int(map[x][y] && map[x-1][y+1]);
-            count_possible_neighbours += 1;
+            count_true_neighbours += bool_to_int(map[x-1][y+1]);
         }
     }
 
-    if y != 10 {
-        count_same_neighbours += bool_to_int(map[x][y] && map[x][y-1]);
-        count_possible_neighbours += 1;
+    if y != 0 {
+        count_true_neighbours += bool_to_int(map[x][y-1]);
     }
 
     if y != HEIGHT-1{
-        count_same_neighbours += bool_to_int(map[x][y] && map[x][y+1]);
-        count_possible_neighbours += 1;
+        count_true_neighbours += bool_to_int(map[x][y+1]);
     }
 
     if x != WIDTH-1{
-        if y != 10 {
-            count_same_neighbours += bool_to_int(map[x][y] && map[x+1][y-1]);
-            count_possible_neighbours += 1;
+        if y != 0 {
+            count_true_neighbours += bool_to_int(map[x+1][y-1]);
         }
 
-        count_same_neighbours += bool_to_int(map[x][y] && map[x+1][y]);
-        count_possible_neighbours += 1;
+        count_true_neighbours += bool_to_int(map[x+1][y]);
 
         if y != HEIGHT-1{
-            count_same_neighbours += bool_to_int(map[x][y] && map[x+1][y+1]);
-            count_possible_neighbours += 1;
+            count_true_neighbours += bool_to_int(map[x+1][y+1]);
         }
     }
 
-    return map[x][y]&&(3*count_same_neighbours >= count_possible_neighbours) ;
+    return count_true_neighbours >= comparaison_level;
 }
 
 /// Generate a tab that represent the map of our game with randomize generation 
-/// and game of life algorithm to make a map more realistic
+/// and cellular automata algorithm to make a map more realistic
 fn generate_map()-> [[bool; HEIGHT] ; WIDTH] {
     let mut seed = random::RandomNumberGenerator::new();
     let mut map_rand = [[false; HEIGHT]; WIDTH];
 
     // generate our first random map 
     for x in 0..WIDTH{
-        for y in 10..HEIGHT{
+        for y in 0..HEIGHT{
             map_rand[x][y] = seed.rand::<i32>() % 2 == 0; 
         }
     }
 
-    // We increase the realism of our generation with a check 
-    // of the neighbours of a pixel to determine if their is enough neighbours 
-    // of the same state around to stay in this state
+    //We increase the realism of our generation with a check 
+    //of the neighbours of a pixel to determine if their is enough neighbours 
+    //with the state true around to stay in this state
     let mut map = [[false; HEIGHT]; WIDTH];
     for x in 0..WIDTH{
-        for y in 10..HEIGHT{
-            map[x][y] = enough_neighbours(x, y, map_rand); //true if their is enough neighbours of the same color
+        for y in 0..HEIGHT{
+            map[x][y] = enough_neighbours(x, y, map_rand, 5); //true if their are enough neighbours that are true
         }
     }
 
     // And to increase the effect of our generation, 
     // we repeat the last operation 
     for x in 0..WIDTH{
-        for y in 10..HEIGHT{
-            map[x][y] = enough_neighbours(x, y, map);
+        for y in 0..HEIGHT{
+            map[x][y] = enough_neighbours(x, y, map, 4);
         }
     }
 
